5c621d3907c747ae9837098db18af205d00d4fc6
Burenkovuser <nb2207@yandex.ru>
2017-02-13 14:06:52 +0600

    tableview started

--- ViewController.m
+++ ViewController.m
@@ -9,15 +9,38 @@
 #import "ViewController.h"
 
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property(weak, nonatomic) UITableView *tableView;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - My metods
+
+-(void) loadView {
+    
+    [super loadView];
+    
+    CGRect frame = self.view.bounds;
+    frame.origin = CGPointZero;// так делают стандартно фрейм
+    
+    // Инициализировали таблицу на нашей вьюхе
+    UITableView* tableView = [[UITableView alloc] initWithFrame:frame style:UITableViewStyleGrouped];//можно установить другой стиль
+    tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    
+    [self.view addSubview:tableView];// добавляем на экран
+    self.tableView = tableView; //добавляем наше проперти
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor redColor];
+    
+    
+    //self.view.backgroundColor = [UIColor redColor];
     // Do any additional setup after loading the view.
 }
 
@@ -35,5 +58,38 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView { //количество секций
+    return 5;
+}
+
+- (nullable NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return [NSString stringWithFormat:@"Section %ld Header", (long)section];
+}
+
+- (nullable NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section {
+    return [NSString stringWithFormat:@"Section %ld Footer", (long)section];
+}
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString* identifier = @"Cell";
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier]; //если менять цвета, шрифт и т.д. лучше делать это здесь
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"Section %ld , Row %ld", indexPath.section, (long)indexPath.row];
+    cell.detailTextLabel.text = @"Value";
+    
+    return cell;
+}
+
 
 @end
